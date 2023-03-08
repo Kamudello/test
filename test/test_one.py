47dd20ac632c74e903b89aff12da212a0f265912
Kamudello <daniil.kelesov2002@gmail.com>
2023-03-09 01:24:44 +0600

    feat: added other files

--- test/test_one.py
+++ test/test_one.py
@@ -4,20 +4,19 @@ from sql_queries import insert_student, get_students
 
 
 def test_service1(conn_with_data: str):
-    assert 2 == 2
-    # engine = create_engine(conn_with_data)
-    # conn = engine.connect()
-    #
-    # school = Student(
-    #     name="Daniil",
-    #     lastname="Kelesov",
-    #     clas="11",
-    #     attendance="Б",
-    #     grade=4
-    # )
-    # insert_student(conn, school)
-    #
-    # transactions = get_students(conn)
-    # assert len(transactions) == 1
-    # transaction = transactions[0]
-    # assert transaction.name == "Daniil"
+    engine = create_engine(conn_with_data)
+    conn = engine.connect()
+
+    school = Student(
+        name="Daniil",
+        lastname="Kelesov",
+        clas="11",
+        attendance="Б",
+        grade=4
+    )
+    insert_student(conn, school)
+
+    transactions = get_students(conn)
+    assert len(transactions) == 1
+    transaction = transactions[0]
+    assert transaction.name == "Daniil"
